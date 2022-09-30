ef9ef8f3caef24294b43bf4cf7d98430b3ce382b
junjiebalatero <junjiebalatero@gmail.com>
2022-09-30 22:16:03 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -4,75 +4,86 @@ import Web3 from "web3";
 
 function App() {
 
-  const [isConnected, setIsConnected] = useState(false);
-  const [ethBalance, setEthBalance] = useState("");
+const [isConnected, setIsConnected] = useState(false);
+const [ethBalance, setEthBalance] = useState("");
 
-  const detectCurrentProvider = () => {
-    let provider;
-    if (window.ethereum) {
-      provider = window.ethereum;
-    } else if (window.web3) {
-      provider = window.web3.currentProvider;
-    } else {
-      console.log("Non-ethereum browser detected. You should install Metamask.");
-    }
-    return provider;
-  };
+const detectCurrentProvider = () => {
+let provider;
+if (window.ethereum) {
+  provider = window.ethereum;
+} else if (window.web3) {
+  provider = window.web3.currentProvider;
+} else {
+  console.log("Non-ethereum browser detected. You should install Metamask.");
+}
+return provider;
+};
 
-  const onConnect = async() => {
-    try {
-      const currentProvider = detectCurrentProvider();
-      if(currentProvider) {
-        await currentProvider.request({method: 'eth_requestAccounts'});
-        const web3 = new Web3(currentProvider);
-        const userAccount = await web3.eth.getAccounts();
-        const account = userAccount[0];
-        let ethBalance = await web3.eth.getBalance(account);
-        setEthBalance(ethBalance);
-        setIsConnected(true);
-      }
-    }catch(err) {
-      console.log(err);
-    }
+const onConnect = async() => {
+try {
+  const currentProvider = detectCurrentProvider();
+  if(currentProvider) {
+    await currentProvider.request({method: 'eth_requestAccounts'});
+    const web3 = new Web3(currentProvider);
+    const userAccount = await web3.eth.getAccounts();
+    const account = userAccount[0];
+    let ethBalance = await web3.eth.getBalance(account);
+    setEthBalance(ethBalance);
+    setIsConnected(true);
   }
+}catch(err) {
+  console.log(err);
+}
+}
 
-  const onDisconnect = () => {
-    setIsConnected(false);
-  }
+const onDisconnect = () => {
+setIsConnected(false);
+}
 
-  return (
-    <div className="app">
-      <div className="app-header"> 
-        <h1>React Dapp athentication with React, Web3.js and Metamask</h1>
+return (
+<div className="app">
+  <div className="app-header"> 
+    <center><h1>Mardz Dapp</h1></center>    
+    <center><h5>When you click and nothing happens that means your browser has no Metamask installed.</h5></center>
+  </div>
+  <div className="app-wrapper">
+    {!isConnected && (
+      <div>
+          <center><button className="app-button__login" onClick={onConnect}>
+          Login
+          </button></center>
       </div>
-      <div className="app-wrapper">
-        {!isConnected && (
+    )}
+  </div>
+  {isConnected && (
+    <div className="app-wrapper">
+      <center>
+        <div className="app-details">
+                  <h2> You are connected to metamask.</h2>
+                      <div className="app-balance">
+                          <span>Balance:  </span>
+                              {ethBalance}
+                </div>
+            </div>          
           <div>
-              <button className="app-button__login" onClick={onConnect}>
-              Login
+             <br />
+              <button className="app-button__logout" onClick={onDisconnect}>
+                Disconnect
               </button>
-          </div>
-        )}
-      </div>
-      {isConnected && (
-        <div className="app-wrapper">
-          <div className="app-details">
-            <h2> You are connected to metamask.</h2>
-              <div className="app-balance">
-                <span>Balance:  </span>
-                  {ethBalance}
-              </div>
-          </div>
-          <div>
-            <button className="app-button__logout" onClick={onDisconnect}>
-              Disconnect
-            </button>
-          </div>
+             </div>
+          <br />
+            <div>
+              <img
+                src="https://gw.crustapps.net/ipfs/QmNgoacgbARoSn9vhAPYdDMzSnsow7dFCfHzeZKftJ1hRD/"
+                alt="junjie"
+              />
         </div>
-      )
-      }
+      </center>
     </div>
-  );
+  )
+  }
+</div>
+);
 }
 
 export default App;

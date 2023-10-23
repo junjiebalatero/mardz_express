fcd1012a4ee6b8a65fd9e4e51b42d702f7c0cdb2
junjiebalatero <junjiebalatero@gmail.com>
2023-10-24 00:17:13 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ return (
   {
     isConnected && (
       <div className="app-wrapper">
-        <>
+        <center>
           <div className="app-details">
               <h3> You are connected to metamask.</h3>
                   <div className="app-balance">
@@ -74,45 +74,53 @@ return (
               <button className="app-button__logout" onClick={onDisconnect}>
                   Disconnect
               </button>
+          </div>    
+          <div>
+            <p>Visit Opensea.io: <a href="https://opensea.io/mardzreal57" target="_blank">Create your own NFT and Sell it for free.</a></p>
           </div>   
-          <h3> Just A Few List of Web 3.0</h3>          
           <div>
-            <p>Visit 4everland.org: <a href="https://7r2gg-ayaaa-aaaag-aaxsq-cai.ic0.app/" >Decentralize your Website, Application, Data and Storage.</a></p>
+            <p>Visit fleek.co: <a href="https://wi6hp-cqaaa-aaaad-qd67q-cai.ic.fleek.co/" target="_blank">Decentralize your Website, Application, Data and Storage.</a></p>
+          </div>    
+          <div>
+            <p>Visit 4everland.org: <a href="https://7r2gg-ayaaa-aaaag-aaxsq-cai.ic0.app/" target="_blank">Decentralize your Website, Application, Data and Storage.</a></p>
           </div>
           <div>
-            <p>Hosted by fleek.co: <a href="https://3sxlk-ayaaa-aaaad-qeagq-cai.ic.fleek.co/" >fleek.co is hosting this app - Connect and Read smart contract</a></p>
+            <p>Hosted by fleek.co: <a href="https://3sxlk-ayaaa-aaaad-qeagq-cai.ic.fleek.co/" target="_blank">fleek.co is hosting this app - Connect and Read smart contract</a></p>
           </div>
           <div>
-            <p>Dsocial - A Decentralized alternate to Youtube: <a href="https://dwqte-viaaa-aaaai-qaufq-cai.ic0.app/@mardz" >@mardz - Watch my Dsocial Channel.</a></p>
+            <p>Dsocial - A Decentralized alternate to Youtube: <a href="https://dwqte-viaaa-aaaai-qaufq-cai.ic0.app/@mardz" target="_blank">@mardz - Watch my Dsocial Channel.</a></p>
           </div>
           <div>
-            <p>Visit nuance: <a href="https://exwqn-uaaaa-aaaaf-qaeaa-cai.ic0.app/" >Blogging to the People.</a></p>
+            <p>Visit nuance: <a href="https://exwqn-uaaaa-aaaaf-qaeaa-cai.ic0.app/" target="_blank">Blogging to the People.</a></p>
           </div>         
           <div>
-            <p>Visit commonwealth.im: <a href="https://commonwealth.im/" >A community for every token.</a></p>
+            <p>Visit commonwealth.im: <a href="https://commonwealth.im/" target="_blank">A community for every token.</a></p>
           </div>         
           <div>
-            <p>distrikt - A Decentralized alternate to Facebook: <a href="https://az5sd-cqaaa-aaaae-aaarq-cai.ic0.app/" >Own your data and identity, make new connections, and build a community.</a></p>
+            <p>distrikt - A Decentralized alternate to Facebook: <a href="https://az5sd-cqaaa-aaaae-aaarq-cai.ic0.app/" target="_blank">Own your data and identity, make new connections, and build a community.</a></p>
           </div>         
           <div>
-            <p>DSCVR - A Decentralized alternate to Reddit: <a href="https://dscvr.one/" >a decentralized social content platform</a></p>
+            <p>DSCVR - A Decentralized alternate to Reddit: <a href="https://dscvr.one/" target="_blank">a decentralized social content platform</a></p>
           </div>
           <div>
-            <p>Dstar Note: <a href="https://nazwh-uiaaa-aaaai-qbexq-cai.ic0.app/#/login" >all data of the notes is stored in the blockchain</a></p>
+            <p>Dstar Note: <a href="https://nazwh-uiaaa-aaaai-qbexq-cai.ic0.app/#/login" target="_blank">all data of the notes is stored in the blockchain</a></p>
           </div>
           <div>
-            <p>Visit Dmail: <a href="https://evyc3-ziaaa-aaaak-aam5a-cai.ic0.app/login" >Decentralize email</a></p>
+            <p>Visit Dmail: <a href="https://evyc3-ziaaa-aaaak-aam5a-cai.ic0.app/login" target="_blank">Decentralize email</a></p>
           </div>
           <div>
-            <p>Visit dbox: <a href="https://2prn7-tqaaa-aaaai-acjja-cai.ic0.app/login" >Decentralize email</a></p>
+            <p>Visit dbox: <a href="https://2prn7-tqaaa-aaaai-acjja-cai.ic0.app/login" target="_blank">Decentralize email</a></p>
           </div>
           <div>
-            <p>Visit tribes.xyz: <a href="https://www.tribes.xyz/" >Decentralize Chat. </a></p>
+            <p>Visit tribes.xyz: <a href="https://www.tribes.xyz/" target="_blank">Decentralize Chat. </a></p>
           </div>
           <div>
-            <p>Visit oc.app: <a href="https://oc.app/" >Decentralize Chat. </a></p>
-          </div>          
-        </>
+            <p>Visit oc.app: <a href="https://oc.app/" target="_blank">Decentralize Chat. </a></p>
+          </div>
+          <div>
+            <p>My code in Github: <a href="https://github.com/junjiebalatero" target="_blank">@junjiebalatero</a></p>
+          </div>
+        </center>
       </div>
     )
   }
